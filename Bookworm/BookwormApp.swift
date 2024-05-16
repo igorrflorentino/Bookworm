f60782f837d28342bafba83f417c6e4244d40490
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-05-16 12:45:23 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Igor Florentino on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
